d2624814ba9c634711192f5c0f9b3dfca72d44d8
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-20 17:56:40 +0200

    Added linkedIn link

--- src/components/InitialPage.jsx
+++ src/components/InitialPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const InitialPage = (props) => {
+    const linkedInUrl = "https://www.linkedin.com/in/victor-mugisha-shyaka-47b10b233/"
     return (
         <div className="initial-page">
             <div className="content">
                 <h1>Quizzical</h1>
-                <p>This is the simple quizz game app!</p>
+                <p>This is the simple quizz game app - Proudly Developed by <a target='_blank' href={linkedInUrl}>Victor M</a>!</p>
                 <button className="button-component" onClick={props.toggleInitial}>Start Quiz</button>
             </div>
         </div>
